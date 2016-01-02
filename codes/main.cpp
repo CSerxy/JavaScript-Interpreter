5f6b2e9061ec453b0d4f64812705b99a94ac4d15
CSerxy <cserxy@gmail.com>
2016-01-02 17:23:53 +0800

    Start to parse input.

---
+++ codes/main.cpp
@@ -0,0 +1,85 @@
+//#include "Variable.h"
+//#include "Backend.h"
+//#include "BuiltinFunctions.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+typedef struct objReturn {
+	string next;
+	int judge;
+	string memory;
+} objReturn;
+
+void getString(string u);
+int weatherContain(string u, string v);
+
+void dealWithFor(string u);
+void dealWithWhile(string u);
+void dealWithIf(string u);
+
+int main() {
+	getString("exit();");
+}
+
+void getString(string u) {
+	string current;
+	while (cin >> current) {
+		if (current == u)
+			return;
+		cout << current;
+		if (current == "for" || weatherContain(current, "for")) {
+			if (current == "for")
+				dealWithFor("");
+			else
+				dealWithFor(current.substr(3));
+
+		}
+		else if (current == "while" || weatherContain(current, "while")) {
+			if (current == "while")
+				dealWithWhile("");
+			else 
+				dealWithWhile(current.substr(5))
+
+		}
+		else if (current == "if" || weatherContain(current, "if")) {
+			if (current == "if")
+				dealWithIf("");
+			else 
+				dealWithIf(current.substr(2))
+
+		}
+
+
+	}
+}
+
+int weatherContain(string u, string v) {
+	if (u.length() <= v.length())
+		return 0;
+	for (int i = 0; i < v.length(); i++) {
+		if (u[i] != v[i])
+			return 0;
+	}
+	return 1;
+}
+
+void dealWithFor(string u) {
+	objReturn k, op1, op2;
+	if (u == "")
+		cin << u;
+	k = dealWithObejct(u, "(", ";", 0, 0);
+	op1 = dealWithObejct(k.next, "", ";", 1, 1);
+	op2 = dealWithObejct(op1.next, "", ")", 0, 1);
+	
+	while (k.judge) {
+		
+	}
+}
+
+void dealWithWhile() {
+	
+}
+void dealWithIf() {
+	
+}
